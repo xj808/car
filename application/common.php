3da603faad4e8796cf65e4e8b53a7172733bff94
zhaizhaohui <3115516997@qq.com>
2018-05-26 10:42:15 +0800

    add host

--- application/common.php
+++ application/common.php
@@ -17,19 +17,19 @@
  * @param  要保存的路径
  * @return 图片保存后的路径
  */
- function upload($image,$path){
-    // 获取表单上传文件 例如上传了001.jpg
+ function upload($image,$path,$host){
+    // 本地测试地址，上线后更改
+    $host = $host ? $host : $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'];
+    // 获取表单上传文件
     $file = request()->file($image);
-    $a='http://192.168.1.110/cyc/public';
-    // 移动到框架应用根目录/uploads/ 目录下
-    $info = $file->move('./uploads/'.$path);
-    if($info){
-        // 成功上传后 获取上传信息
-        // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-        return  $a.'/uploads/'.$path.'/'.$info->getSaveName();
+    // 进行验证并进行上传
+    $info = $file->validate(['size'=>15678,'ext'=>'jpg,png,jpeg'])->move( './uploads'.$path);
+    // 上传成功后输出信息
+    if($ifno){
+      return  $a.'/uploads/'.$path.'/'.$info->getSaveName();
     }else{
-        // 上传失败获取错误信息
-        return  $file->getError();
+      // 上传失败获取错误信息
+      return  $file->getError();
     }
 }
 
