ca63130661d2816afaea52f593db79e71d74a0e7
chenjiaxin0114 <1317250720@qq.com>
2018-06-08 11:18:22 +0800

    v1.0.25

--- application/admin/controller/SmallInvoice.php
+++ application/admin/controller/SmallInvoice.php
@@ -8,8 +8,34 @@ use think\Db;
 class SmallInvoice extends Admin
 {
 	
-	public function index()
+	/**
+	 * 申请列表
+	 * @return [type] [description]
+	 */
+	public function applyList()
 	{
-		$page = input('post.page');
+		$page = input('post.page')? : 1;
+		$this->index($page,0);
 	}
+
+
+	// 列表
+	private function index($page,$status)
+	{
+		$pageSize = 10;
+		$count = Db::table('u_tax')->where('status',$status)->count();
+		$rows = ceil($count / $pageSize);
+		$list = Db::table('u_tax')
+				->where('status',$status)
+				->order('id desc')
+				->page($page,$pageSize)
+				->field('id,phone,contacter,total,fee,create_time,audit_time')
+				->select();
+		if($count > 0){
+            $this->result(['list'=>$list,'rows'=>$rows],1,'获取列表成功');
+        }else{
+            $this->result('',0,'暂无数据');
+        }  
+	}
+
 }
