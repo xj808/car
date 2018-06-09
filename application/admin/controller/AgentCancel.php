1d2a5d1f7ef5ccecc2f158f6521426f1f6c94649
chenjiaxin0114 <1317250720@qq.com>
2018-06-09 18:08:40 +0800

    v1.0.28

--- application/admin/controller/AgentCancel.php
+++ application/admin/controller/AgentCancel.php
@@ -68,8 +68,145 @@ class AgentCancel extends Admin
 
 
 
+	public function adopt()
+	{
+		// 获取取消合作订单id、获取运营商id、获取取消合作理由、获取运营商名称
+		$data = input('post.');
+		Db::startTrans();
+		// 修改运营商旗下修车厂状态为取消合作后继续做邦保养,小程序不显示。
+		$audit_status = Db::table('cs_shop')->where('aid',$data['aid'])->setField('audit_status',6);
+		if($audit_status !== false){
+
+			// 修车厂除去用户的油，剩余油品。把所有修车厂都录入到修车厂取消合作表里,给运营商增加授信库存
+			$shop_cancel = $this->shopCancel($data['aid']);
+			if($shop_cancel !==false){
+				// 获取运营商现有库存，入库到运营商取消合作表
+				$agent_ration =Db::table('ca_ration cr')
+								->join('co_bang_cate bc','cr.materiel = bc.id')
+								->where('aid',$data['aid'])->field('sid,materiel,ration,stock,name')->select();
+				$arr['detail']=$agent_ration;
+				$result = Db::table('ca_apply_cancel')->json(['detail'])->insert($arr);
+				if($result){
+					// 清除运营商的库存，修改运营商为取消合作。
+					$res = Db::table('ca_agent')->where('aid',$data['aid'])->setField();
+				}else{
+					Db::rollback();
+					$this->result('',0,'运营商物料详情失败');
+				}
+			}else{
+				Db::rollback();
+				$this->result('',0,$shop_cancel);
+			}
+
+			
+		}else{
+			Db::rollback();
+			$this->result('',0,'更改修车厂取消合作失败');
+		}
+		// 给取消合作运营商下的修车厂发送短信。
+		
+		// 运营商库存回收所有修车厂物料
+		// 总后台回收所有运营商库存
+	}
 
 
+
+
+	public function shopCancel($aid)
+	{
+		// 获取修车厂信息
+		$shop = Db::table('cs_shop')->where('aid',$aid)->field('id,aid,company,leader,reason')->select();
+		foreach ($shop as $k => $v) {
+			$arr=[
+				'sid'=>$v['id'],//修车厂id
+				'aid'=>$v['aid'],//运营商id
+				'company'=>$v['company'],//修车厂名称
+				'leader'=>$v['leader'],//修车厂负责人
+				'reason'=>'运营商取消合作。',//修车厂取消合作原因
+			];
+			$res = Db::table('cs_apply_cancel')->insertGetId($arr);
+			$r[]=$res;
+		}
+		if($res !== false){
+			// 获取每个修车厂的库存
+			$shop_ration = $this->surRation($shop);
+			foreach ($shop_ration as $k => $v) {
+				$where=['aid'=>$aid,'materiel'=>$v['materiel']];
+				// 运营商减少补货库存（给授信库增加）
+				$materiel_stock=$v['ration']-$v['stock'];
+				// 增加授信库
+				$open = Db::table('ca_ration')->where($where)->inc('open_stock',$v['stock']+$materiel_stock)->update();
+				// 减少补给库数量补充给授信库
+				$result = Db::table('ca_ration')->where($where)->dec('materiel_stock',$materiel_stock)->update();
+			}
+			if($result !== false){
+				return true;
+
+			}else{
+				return '增加运营商授信库失败';
+			}
+
+		}else{
+			return '修车厂的取消合作失败';
+		}
+	}
+
+
+	/**
+	 * 获取修车厂除去需要做邦保养的油剩余的油是多少;
+	 * @param  [type] $data [description]
+	 * @return [type]       [description]
+	 */
+	public function surRation($data)
+	{
+		// 获取每个修车厂的所有用户
+		foreach ($data as $k => $v) {
+			 $list = Db::table('u_card uc')
+					->join('u_user uu','uc.uid = uu.id')
+					->join('co_bang_data bd','uc.car_cate_id = bd.cid')
+					->where([['uc.sid','=',$v['id']],['uc.remain_times','>',0]])
+					->field('sid,phone,name,oil_name,litre,plate,uc.oil,remain_times')
+					->select();
+			$user_num[] = $list;
+		}
+
+		foreach ($user_num as $ke => $v) {
+			foreach($v as $key=>$value){
+				$arr[] = $value;
+			}
+		}
+
+		// 获取修车厂现有库存，判断留油之后还剩多少库存
+		$ration = $this->shopRation($data);
+		foreach ($ration as $kk => $vv) {
+			foreach($arr as $kkk=>$vvv){
+				if($vv['sid'] == $vvv['sid'] && $vv['materiel'] == $vvv['oil']){
+					// 修改修车厂的库存，为仅做邦保养的库存
+					Db::table('cs_ration')->where('sid',$vvv['sid'])->setField('stock',0);
+					Db::table('cs_ration')->where(['sid'=>$vvv['sid'],'materiel'=>$vvv['remain_times']])->setField('stock',$vvv['remain_times']*$vvv['litre']);
+					$ration[$kk]['stock'] = $vv['stock']-$vvv['remain_times']*$vvv['litre'];
+					
+				}
+			}
+		}
+		return $ration;
+	}
+
+
+
+	public function shopRation($data)
+	{
+		foreach ($data as $k => $v) {
+			$list = Db::table('cs_ration')->where('sid',$v['id'])->select();
+			$arr[] = $list;
+		}
+		foreach ($arr as $ke => $va) {
+			foreach ($va as $key => $value) {
+				$ration[] = $value;
+			}
+		}
+		return $ration;
+	}
 
 	/**
 	 * 获取运营商取消合作列表
